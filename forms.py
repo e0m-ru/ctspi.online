77df880b81a1937d744682b77565fb9323ebeec2
Aleksandr Babushkin <i@e0m.ru>
2024-06-04 19:46:53 +0300

    Form Wizard

--- forms.py
+++ forms.py
@@ -1,4 +1,64 @@
+from formtools.wizard.views import SessionWizardView
+from django.http import HttpResponseRedirect
 from django import forms
+from datetime import datetime
+
 
 class SearchForm(forms.Form):
     query = forms.CharField(label="поиск")
+
+
+# input type = "datetime-local"
+class DateTimeLocalInput(forms.DateTimeInput):
+    input_type = "datetime-local"
+
+
+class DateTimeLocalField(forms.DateTimeField):
+    # Set DATETIME_INPUT_FORMATS here because, if USE_L10N
+    # is True, the locale-dictated format will be applied
+    # instead of settings.DATETIME_INPUT_FORMATS.
+    # See also:
+    # https://developer.mozilla.org/en-US/docs/Web/HTML/Date_and_time_formats
+    input_formats = [
+        "%Y-%m-%dT%H:%M:%S",
+        "%Y-%m-%dT%H:%M:%S.%f",
+        "%Y-%m-%dT%H:%M",
+    ]
+    widget = DateTimeLocalInput(format="%Y-%m-%dT%H:%M")
+
+
+class EventTitle(forms.Form):
+    # form 1
+    title = forms.CharField(
+        label='Заголовок',
+        max_length=100,
+        min_length=3,
+        strip=True,
+    )
+    
+
+class EventTitle2(forms.Form):
+    date_Time = DateTimeLocalField(
+        required=bool,
+        label='Дата и время',
+        initial=datetime.now(),
+        
+    )
+
+
+class EventDescription(forms.Form):
+    # form 2
+    description = forms.CharField(
+        widget=forms.Textarea(),
+    )
+
+
+class EventWizard(SessionWizardView):
+    template_name = "EventWizardForm.html"
+    form_list = [EventTitle, EventTitle2, EventDescription]
+
+    def done(self, form_list, **kwargs):
+        form_data = [form.cleaned_data for form in form_list]
+        # обработка данных
+
+        return HttpResponseRedirect('/finished/')
--- urls.py
+++ urls.py
@@ -1,20 +1,22 @@
 from django.urls import re_path, path, include
 from ctspi import views
+from ctspi.forms import EventTitle, EventDescription, EventWizard
 
 urlpatterns = [
-    path("accounts/", include("django.contrib.auth.urls")),
-    re_path(r'departments/.*', views.departments),
-    re_path(r'blog/.*', views.blog, name='event-list'),
-    path('search/', views.event_search, name='post_search'),
-    path('player/', views.player, name='player'),
-    # CRUD
-    path('event/new/', views.EventCreateView.as_view(), name='event-create'),
-    path('event/<int:pk>/', views.EventDetailView.as_view(), name='event-detail'),
-    path('event/<int:pk>/update/',
-         views.EventUpdateView.as_view(), name='event-update'),
-    path('event/<int:pk>/delete/',
-         views.EventDeleteView.as_view(), name='event-delete'),
+     path("accounts/", include("django.contrib.auth.urls")),
+     re_path(r'departments/.*', views.departments),
+     re_path(r'blog/.*', views.blog, name='event-list'),
+     path('search/', views.event_search, name='post_search'),
+     path('player/', views.player, name='player'),
+     # CRUD
+     path('event/new/', views.EventCreateView.as_view(), name='event-create'),
+     path('event/<int:pk>/', views.EventDetailView.as_view(), name='event-detail'),
+     path('event/<int:pk>/update/',
+          views.EventUpdateView.as_view(), name='event-update'),
+     path('event/<int:pk>/delete/',
+          views.EventDeleteView.as_view(), name='event-delete'),
+    path('new_event/', EventWizard.as_view()),
 
-    re_path(r'\w*', views.main),
-    re_path(r'.*', views.ctspi_404),
+     re_path(r'\w*', views.main),
+     re_path(r'.*', views.ctspi_404),
 ]
